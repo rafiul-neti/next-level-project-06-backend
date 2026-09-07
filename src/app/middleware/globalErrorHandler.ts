39c759369e0faa32ea6bfe937f63e5a413e0bf44
Rafiul Islam <irafiul210@gmail.com>
2026-09-07 15:04:22 +0600

    fix: consoling errors in deployment phase

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -12,9 +12,7 @@ export const globalErrorHandler = async (
   res: Response,
   _next: NextFunction,
 ) => {
-  if (config.node_env === "development") {
-    console.log("Error from Global Error Handler", err);
-  }
+  console.log("Error from Global Error Handler", err);
 
   let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
   let errorMessage = err.message || "Internal Server Error";
